14d4ba3e55eafcebd7fff786643bdf5f47a8d9bb
Nick M Cummins <nickmcummins@gmail.com>
2025-11-24 11:34:48 -0800

    added TruckLib.CLI project

---
+++ TruckLib.CLI/Program.cs
@@ -0,0 +1,17 @@
+﻿using TruckLib.HashFs;
+using TruckLib.ScsMap;
+
+namespace TruckLib.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mapFilename = args[0];
+            IHashFsReader reader = HashFsReader.Open(mapFilename);
+
+            Map map = Map.Open("/map/usa.mbd", reader);
+            Console.Out.WriteLine(map);
+        }
+    }
+}
